efff6d05e69cded9e1ced6eef83aeea8870307d2
Alex Ware <alex.ware@guardian.co.uk>
2019-04-23 21:28:51 +0100

    fix lint

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,4 +1,3 @@
-import PropTypes from "prop-types"
 import React from "react"
 import styles from "./timeline.module.scss"
 export const Timeline = ({ children }) => <div className={styles.timeline}>{children}</div>
