21268b0be49060b85db6ad4bc0004f668823c38d
Alex Ware <alex.ware@guardian.co.uk>
2019-04-23 21:02:54 +0100

    change title

--- src/components/seo.js
+++ src/components/seo.js
@@ -32,8 +32,7 @@ function SEO({ description, lang, meta, keywords, title }) {
       htmlAttributes={{
         lang,
       }}
-      title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
+      title={site.siteMetadata.title}
       meta={[
         {
           name: `description`,
