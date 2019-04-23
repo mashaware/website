0250c4b05d58c36a2900bf8f409871a02fc2dbed
Alex Ware <alex.ware@guardian.co.uk>
2019-04-23 23:04:47 +0100

    fix graphql

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,68 +3,69 @@ import styles from "./hero.module.scss"
 import { ContactLogos } from "./contactLogos"
 import { useStaticQuery, graphql } from "gatsby"
 
+const getSrc = _ => _.childImageSharp.fixed.src
 const Anim = () => {
-  const {face, book, wireframes, mug, pen, sitemap, stickies, } = useStaticQuery(
+  const {face:face, book, wireframes, mug, pen, sitemap, stickies, } = useStaticQuery(
      graphql`
-fragment imageQuery on File {
+fragment imageQ on File {
   childImageSharp {
-    fixed(width: "200px") {
+    fixed(width: 200) {
       ...GatsbyImageSharpFixed
     }
   }
 }
 
 {
-  face: file(relativePath: {eq: "../images/my face.png"}) {
-    ...imageQuery
+  face: file(relativePath: {eq: "my face.png"}) {
+    ...imageQ
   }
-  book: file(relativePath: {eq: "../images/book.png"}) {
-    ...imageQuery
+  book: file(relativePath: {eq: "book.png"}) {
+    ...imageQ
   }
-  wireframes: file(relativePath: {eq: "../images/wireframes.png"}) {
-    ...imageQuery
+  wireframes: file(relativePath: {eq: "wireframes.png"}) {
+    ...imageQ
   }
-  mug: file(relativePath: {eq: "../images/mug.png"}) {
-    ...imageQuery
+  mug: file(relativePath: {eq: "mug.png"}) {
+    ...imageQ
   }
-  pen: file(relativePath: {eq: "../images/pen.png"}) {
-    ...imageQuery
+  pen: file(relativePath: {eq: "pen.png"}) {
+    ...imageQ
   }
-  sitemap: file(relativePath: {eq: "../images/sitemap.png"}) {
-    ...imageQuery
+  sitemap: file(relativePath: {eq: "sitemap.png"}) {
+    ...imageQ
   }
-  stickies: file(relativePath: {eq: "../images/stickies.png"}) {
-    ...imageQuery
+  stickies: file(relativePath: {eq: "stickies.png"}) {
+    ...imageQ
   }
 }
  `)
+ console.log(face)
   return (
     <div className={styles.wrapper}>
       <div className={styles.hero}>
-        <img
-          src={face}
+        <img  src={getSrc(face)}
           className={styles.face}
           alt="Illustration of my face."
         />
-        <img src={book} className={styles.book} alt="Illustration of a book" />
-        <img src={pen} className={styles.pen} alt="Illustration of a pen" />
-        <img
-          src={wireframes}
+        <img  src={getSrc(book)} className={styles.book} alt="Illustration of a book" />
+        <img  src={getSrc(pen)} className={styles.pen} alt="Illustration of a pen" />
+        <img 
+           src={getSrc(wireframes)}
           className={styles.wireframes}
           alt="Illustration of some wireframes"
         />
         <img
-          src={stickies}
+           src={getSrc(stickies)}
           className={styles.stickies}
           alt="Illustration of some sticky notes."
         />
         <img
-          src={sitemap}
+           src={getSrc(sitemap)}
           className={styles.sitemap}
           alt="Illustration of a sitemap."
         />
         <img
-          src={mug}
+           src={getSrc(mug)}
           className={styles.mug}
           alt="Illustration of a mug of tea."
         />{" "}
