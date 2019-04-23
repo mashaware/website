bd62de68710c3740184218f90de5dc168ddc6590
Alex Ware <alex.ware@guardian.co.uk>
2019-04-23 23:05:25 +0100

    fix target blank

--- src/components/contactLogos.js
+++ src/components/contactLogos.js
@@ -1,43 +1,52 @@
 import React from "react"
 import styles from "./contactLogos.module.css"
 import { useStaticQuery, graphql } from "gatsby"
-import  Img  from 'gatsby-image';
- const query = graphql`
-   fragment imageQuery on File {
-     childImageSharp {
-       fixed(width: 30, height: 30) {
-         ...GatsbyImageSharpFixed
-       }
-     }
-   }
-   query {
-     linkedin: file(relativePath: { eq: "logos/linkedin.png" }) {
-       ...imageQuery
-     }
-     gmail: file(relativePath: { eq: "logos/gmail.png" }) {
-       ...imageQuery
-     }
-     medium: file(relativePath: { eq: "logos/medium.png" }) {
-       ...imageQuery
-     }
-   }
- `
- 
-export const ContactLogos = () => {
-    const {linkedin, gmail,medium} = useStaticQuery(
-        query
-           )   
-    return (
-      <div className={styles.logos}>
-        <a href="mailto:&#109;&#097;&#115;&#104;&#097;&#119;&#097;&#114;&#101;&#046;&#117;&#120;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;">
-          <Img fixed={gmail.childImageSharp.fixed} alt="send a message to my gmail account" />
-        </a>
-        <a href="https://www.linkedin.com/in/mashaware/" target="_blank">
-          <Img fixed={linkedin.childImageSharp.fixed} alt="my linkedin profile" />
-        </a>
-        <a href="https://medium.com/@mashaware" target="_blank">
-          <Img fixed={medium.childImageSharp.fixed} alt="my posts on medium" />
-        </a> 
-      </div>
-    )
+import Img from "gatsby-image"
+const query = graphql`
+  fragment imageQuery on File {
+    childImageSharp {
+      fixed(width: 30, height: 30) {
+        ...GatsbyImageSharpFixed
+      }
     }
+  }
+  query {
+    linkedin: file(relativePath: { eq: "logos/linkedin.png" }) {
+      ...imageQuery
+    }
+    gmail: file(relativePath: { eq: "logos/gmail.png" }) {
+      ...imageQuery
+    }
+    medium: file(relativePath: { eq: "logos/medium.png" }) {
+      ...imageQuery
+    }
+  }
+`
+
+export const ContactLogos = () => {
+  const { linkedin, gmail, medium } = useStaticQuery(query)
+  return (
+    <div className={styles.logos}>
+      <a href="mailto:&#109;&#097;&#115;&#104;&#097;&#119;&#097;&#114;&#101;&#046;&#117;&#120;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;">
+        <Img
+          fixed={gmail.childImageSharp.fixed}
+          alt="send a message to my gmail account"
+        />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/mashaware/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Img fixed={linkedin.childImageSharp.fixed} alt="my linkedin profile" />
+      </a>
+      <a
+        href="https://medium.com/@mashaware"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Img fixed={medium.childImageSharp.fixed} alt="my posts on medium" />
+      </a>
+    </div>
+  )
+}
