b12afa3ffc3eefde219e2eb43f53cc2efb89e7e2
Alex Ware <alex.ware@guardian.co.uk>
2019-04-23 23:13:11 +0100

    remove placeholder text

--- src/pages/success.js
+++ src/pages/success.js
@@ -7,8 +7,8 @@ import { Section } from "../components/section";
 const SecondPage = () => (
   <Layout>
     <Section name="success" colour="yellowgreen">
-    <h1>Form sent succesfully</h1>
-    <p>tktktkå</p>
+    <h1>Thank you for your message!</h1>
+     
     <Link to="/">Go back to the homepage</Link>
     </Section>
   </Layout>
