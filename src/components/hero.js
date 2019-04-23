c99c43cdccdcb8d6582d328ecbb56de119b699da
Alex Ware <alex.ware@guardian.co.uk>
2019-04-23 22:40:45 +0100

    use graphql for image

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,25 +1,59 @@
 import React from "react"
-import face from "../images/my face.png"
-import book from "../images/book.png"
-import wireframes from "../images/wireframes.png"
-import mug from "../images/mug.png"
-import pen from "../images/pen.png"
-import sitemap from "../images/sitemap.png"
-import stickies from "../images/stickies.png"
 import styles from "./hero.module.scss"
 import { ContactLogos } from "./contactLogos"
+import { useStaticQuery, graphql } from "gatsby"
 
 const Anim = () => {
+  const {face, book, wireframes, mug, pen, sitemap, stickies, } = useStaticQuery(
+     graphql`
+   fragment imageQuery on File {
+     childImageSharp {
+       fixed(width: 200px) {
+         ...GatsbyImageSharpFixed
+       }
+     }
+   }
+   query {
+face: file(relativePath:  { eq: "../images/my face.png" }) {...imageQuery}
+book: file(relativePath:  { eq: "../images/book.png" }) {...imageQuery}
+wireframes: file(relativePath:  { eq: "../images/wireframes.png" }) {...imageQuery}
+mug: file(relativePath:  { eq: "../images/mug.png" }) {...imageQuery}
+pen: file(relativePath:  { eq: "../images/pen.png" }) {...imageQuery}
+sitemap: file(relativePath:  { eq: "../images/sitemap.png" }) {...imageQuery}
+stickies: file(relativePath:  { eq: "../images/stickies.png" }) {...imageQuery}
+
+   }
+ `)
   return (
     <div className={styles.wrapper}>
       <div className={styles.hero}>
-        <img src={face} className={styles.face} alt="Illustration of my face."/>
-        <img src={book} className={styles.book} alt="Illustration of a book"/> 
+        <img
+          src={face}
+          className={styles.face}
+          alt="Illustration of my face."
+        />
+        <img src={book} className={styles.book} alt="Illustration of a book" />
         <img src={pen} className={styles.pen} alt="Illustration of a pen" />
-        <img src={wireframes} className={styles.wireframes} alt="Illustration of some wireframes"/>
-        <img src={stickies} className={styles.stickies} alt="Illustration of some sticky notes." />
-        <img src={sitemap} className={styles.sitemap} alt="Illustration of a sitemap." />
-        <img src={mug} className={styles.mug} alt="Illustration of a mug of tea." />{" "}
+        <img
+          src={wireframes}
+          className={styles.wireframes}
+          alt="Illustration of some wireframes"
+        />
+        <img
+          src={stickies}
+          className={styles.stickies}
+          alt="Illustration of some sticky notes."
+        />
+        <img
+          src={sitemap}
+          className={styles.sitemap}
+          alt="Illustration of a sitemap."
+        />
+        <img
+          src={mug}
+          className={styles.mug}
+          alt="Illustration of a mug of tea."
+        />{" "}
       </div>
     </div>
   )
@@ -31,6 +65,6 @@ export const Hero = () => (
       teaching and psychology ready to disrupt learning.
       <ContactLogos />
     </div>
-      <Anim />
+    <Anim />
   </div>
 )
