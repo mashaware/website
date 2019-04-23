6c9051521d333cacae25427414fd8b7e8a27b1c9
Alex Ware <alex.ware@guardian.co.uk>
2019-04-23 21:31:08 +0100

    Add project

--- src/components/projects.js
+++ src/components/projects.js
@@ -4,27 +4,30 @@ import styles from "./projects.module.scss"
 
 export const Projects = () => (
   <div className={styles.container}>
-      <Circle colour="pink">
-        <strong>
-          Mastercard <br />
-          live client
-          <br />
-        </strong>
-        group
-        <br />
-        onboarding
-      </Circle>
-      <Circle colour="yellow">
-        <strong>
-          Ski Deal Comparison
-          <br />
-          live client
-          <br />
-        </strong>
-        group
-        <br />
-        e-commerce
-      </Circle>
-      <Circle colour="orange">tabby cat</Circle>
+    <Circle colour="pink">
+      <strong>Mastercard</strong> <br />
+      live client
+      <br />
+      group
+      <br />
+      onboarding
+    </Circle>
+    <Circle colour="yellow">
+      <strong>Ski Deal Comparison</strong>
+      <br />
+      live client
+      <br />
+      group
+      <br />
+      e-commerce
+    </Circle>
+    <Circle colour="orange">
+      {" "}
+      <strong>BXe /ˈbɪzi/</strong>
+      <br />
+      concept project <br />
+      solo <br />
+      gardening organiser
+    </Circle>
   </div>
 )
